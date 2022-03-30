4c6c3812f1d70f1a921ec188465a4d406a9659d6
IthilNah <alankasis@hotmail.com>
2022-03-30 20:29:55 +0100

    30-3 v2

--- src/components/CheckoutForm/PaymentForm.jsx
+++ src/components/CheckoutForm/PaymentForm.jsx
@@ -5,52 +5,48 @@ import { loadStripe } from '@stripe/stripe-js';
 import {Transactions} from '../Blockchain/Transactions'
 import Review from './Review';
 
-const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUBLIC_KEY);
+// const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUBLIC_KEY);
 
 const PaymentForm = ({ checkoutToken, nextStep, backStep, shippingData, onCaptureCheckout }) => {
 
 
 
-  const handleSubmit = async (event, elements, stripe) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
 
-    if (!stripe || !elements) return;
+    // if (!stripe || !elements) return;
 
-    const cardElement = elements.getElement(CardElement);
+    // const cardElement = elements.getElement(CardElement);
 
-    const { error, paymentMethod } = await stripe.createPaymentMethod({ type: 'card', card: cardElement });
+    // const { error, paymentMethod } = await stripe.createPaymentMethod({ type: 'card', card: cardElement });
 
-    if (error) {
-      console.log('[error]', error);
-    } else {
-      const orderData = {
-        line_items: checkoutToken.live.line_items,
-        customer: { firstname: shippingData.firstName, lastname: shippingData.lastName, email: shippingData.email },
-        shipping: { name: 'International', street: shippingData.address1, town_city: shippingData.city, county_state: shippingData.shippingSubdivision, postal_zip_code: shippingData.zip, country: shippingData.shippingCountry },
-        fulfillment: { shipping_method: shippingData.shippingOption },
-        payment: {
-          gateway: 'stripe',
-          stripe: {
-            payment_method_id: paymentMethod.id,
-          },
-        },
-      };
+    // if (error) {
+    //   console.log('[error]', error);
+    // } else {
+    const orderData = {
+      line_items: checkoutToken.live.line_items,
+      customer: { firstname: shippingData.firstName, lastname: shippingData.lastName, email: shippingData.email },
+      shipping: { name: 'International', street: shippingData.address1, town_city: shippingData.city, county_state: shippingData.shippingSubdivision, postal_zip_code: shippingData.zip, country: shippingData.shippingCountry },
+      fulfillment: { shipping_method: shippingData.shippingOption },
+      // payment: {
+      //   gateway: 'stripe',
+      //   stripe: {
+      //     payment_method_id: paymentMethod.id,
+      //   },
+      // },
+    };
 
       onCaptureCheckout(checkoutToken.id, orderData);
 
       nextStep();
-    }
   };
 
   return (
     <>
       <Review checkoutToken={checkoutToken} />
       <Divider />
-      <Typography variant="h6" gutterBottom style={{ margin: '20px 0' }}>Payment method</Typography>
-      <Elements stripe={stripePromise}>
-        <ElementsConsumer>{({ elements, stripe }) => (
-          <form onSubmit={(e) => handleSubmit(e, elements, stripe)}>
-            <CardElement />
+      {/* <Typography variant="h6" gutterBottom style={{ margin: '20px 0' }}>Pay with $SAMOT</Typography> */}
+          <form onSubmit={(e) => handleSubmit(e)}>
             <br /> <br />
             <div style={{ display: 'flex', justifyContent: 'space-between' }}>
               <Button variant="outlined" onClick={backStep}>Back</Button>
@@ -59,11 +55,8 @@ const PaymentForm = ({ checkoutToken, nextStep, backStep, shippingData, onCaptur
                 </Transactions>
             </div>
           </form>
-        )}
-        </ElementsConsumer>
-      </Elements>
     </>
-  );
+  )
 };
 
 export default PaymentForm;
